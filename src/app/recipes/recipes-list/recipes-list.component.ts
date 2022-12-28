9f6e0c8aad54edaa54bbd4c36bd4dd255505675f
staraneelpatra <staraneel@live.com>
2022-12-28 21:50:48 +0530

    Adding Content to Recipes Component UI things

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+  recpies : Recipe[] = [
+    new Recipe('A Test Recipe',"This is simply a Test",
+    "https://i0.wp.com/pixahive.com/wp-content/uploads/2020/10/Biryani-151674-pixahive.jpg?fit=778%2C855&ssl=1")
+  ];
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
