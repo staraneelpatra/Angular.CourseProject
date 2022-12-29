fe53907e25eb682b8e3c0259b30bf2bc1bbf87e1
staraneelpatra <staraneel@live.com>
2022-12-29 23:11:03 +0530

    Displaying Recipe Details

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
   recpies : Recipe[] = [
     new Recipe('A Test Recipe',"This is simply a Test",
+    "https://i0.wp.com/pixahive.com/wp-content/uploads/2020/10/Biryani-151674-pixahive.jpg"),
+    new Recipe('A Test Recipe',"This is simply a Test",
     "https://i0.wp.com/pixahive.com/wp-content/uploads/2020/10/Biryani-151674-pixahive.jpg")
   ];
   ngOnInit(): void {
