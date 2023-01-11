c0caaf91d9769d4db26d673a6aafd29c49eec1db
staraneelpatra <staraneel@live.com>
2023-01-11 15:24:39 +0530

    Moving recipedata from recip list to recipe item

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent {
-
+@Input() recipe :Recipe ;
 }
